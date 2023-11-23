7c58ab39dd72ff62793fffef2870ea5c63529150
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-23 15:10:33 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-22@17da3ae922184d5a3f17b1e604db53e3ddd39eac 🚀

--- index.782f97a3.js
+++
@@ -1,2 +0,0 @@
-var URL="https://jsonplaceholder.typicode.com/posts",newPost={userId:101,title:"My new Post",body:"lsdkjfkdlsjfsdlkfj lskdjflskdjfsdlkfj lskdjfldksfj"};fetch(URL,{method:"POST",body:JSON.stringify(newPost),headers:{"Content-Type":"application/json"}});
-//# sourceMappingURL=index.782f97a3.js.map
